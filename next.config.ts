459095d5269fab086641d45593355ed042d293bc
ammar3172 <a3767257@gmail.com>
2026-06-11 20:54:25 +0300

    fix: turbopack root config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: { root: '.' },
   experimental: {
     optimizePackageImports: ['@supabase/supabase-js'],
+  },
+  turbopack: {
+    root: process.cwd(),
   },
 };
 
