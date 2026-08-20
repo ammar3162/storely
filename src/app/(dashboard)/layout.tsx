a2e24740b67ac3925c2c411d16d39a3f99554524
ammar3172 <a3767257@gmail.com>
2026-08-21 00:47:19 +0300

    revert: sidebar item label back to 'المنيو الإلكتروني' per request, instead of 'المنتجات'

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -44,7 +44,7 @@ const NAV_GROUPS = [
   {
     label: 'المتجر',
     items: [
-      { href:'/online-store', label:'المنتجات', icon:'M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' },
+      { href:'/online-store', label:'المنيو الإلكتروني', icon:'M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' },
       { href:'/reservation-settings', label:'الحجوزات', icon:'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' },
       { href:'/addons-market', label:'الإضافات', icon:'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4' },
     ]
